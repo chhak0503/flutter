60c122697d7c7812c7a42b9c9433c284cc90bbfa
chhak0503 <chhak0503@gmail.com>
2025-10-24 10:49:01 +0900

    add ch01

--- ch01/11_future_async_await.dart
+++ ch01/11_future_async_await.dart
@@ -4,6 +4,8 @@
   내용 : Dart 비동기 처리
 */
 
+import 'dart:async';
+
 // Future는 미래에 가져오는 값을 의미하는 객체
 Future<String> fetchData(){
   // 비동기 작업 정의
@@ -12,21 +14,31 @@ Future<String> fetchData(){
   });
 }
 
+// Stream을 반환하는 함수
+Stream<int> countStream() async* {
+
+  for(int i=1 ; i<=5 ; i++){
+    await Future.delayed(Duration(seconds: 1));
+    yield i; // 스트림으로 데이터는 내보내기(emit)
+  }
+}
+
+
 void main() async {
 
   // Future 비동기 작업 수행
   print('here...1');
 
   fetchData()
-      .then((data){
-        print('here...2 : $data');
-      })
-      .catchError((err){
-        print('here...3 : $err');
-      })
-      .whenComplete((){
-        print('here...4 비동기 작업 완료');
-      });
+    .then((data){
+      print('here...2 : $data');
+    })
+    .catchError((err){
+      print('here...3 : $err');
+    })
+    .whenComplete((){
+      print('here...4 비동기 작업 완료');
+    });
 
   print('here...5');
 
@@ -44,4 +56,35 @@ void main() async {
 
   print('async_await...5');
 
+
+  /*
+    Future vs Stream
+     - Future는 한 번만 비동기 값을 반환하는 비동기 처리
+     - Stream은 시간이 지남에 따라 여러 데이터를 순차적으로 전달하는 비동기 처리
+  */
+  // Stream 작업
+  final controller = StreamController<String>(); // String은 스트림으로 보내는 데이터 타입
+
+  // Stream 리스너 등록
+  controller.stream
+    .listen((data){
+      print('stream...1 : $data');
+
+
+    })
+    .onError((err){
+      print('stream error');
+    });
+
+  // 스트림 데이터 전송
+  controller.sink.add('hello');
+  controller.sink.add('welcome');
+  controller.sink.add('greeting');
+
+  // Stream 함수 호출 후 await for 출력
+  Stream<int> stream = countStream();
+
+  await for(int number in stream){
+    print('스트림 데이터 수신 : $number');
+  }
 }
