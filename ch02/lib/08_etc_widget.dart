358981658ce8725d8685ecee953aa8f0edf9a9ba
chhak0503 <chhak0503@gmail.com>
2025-10-24 14:49:15 +0900

    add ch02

--- ch02/lib/08_etc_widget.dart
+++ ch02/lib/08_etc_widget.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Hello Flutter!'),
+      home: const MyHomePage(title: '08.기타 나머지 위젯 실습'),
     );
   }
 }
@@ -34,15 +34,48 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Hello Flutter!'),
-            Text('Hello World!')
-          ],
-        ),
-      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Text Left!!!', style: TextStyle(fontSize: 26)),
+          
+          // Center : 위젯을 가운데로 정렬
+          Center(
+            child: Text('Text Center!!!', style: TextStyle(fontSize: 26)),
+          ),
+
+          // Expanded : 비율로 배치
+          Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(
+                  width: 50,
+                  height: 100,
+                  color: Colors.red,
+                )
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  width: 50,
+                  height: 100,
+                  color: Colors.green,
+                )
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  width: 50,
+                  height: 100,
+                  color: Colors.blue,
+                )
+              )
+            ],
+          ),
+
+        ],
+      )
       
     );
   }
