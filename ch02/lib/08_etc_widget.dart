85c0a484337a5920d80a7d4577b5997d867b2ca8
chhak0503 <chhak0503@gmail.com>
2025-10-24 15:46:52 +0900

    add ch02

--- ch02/lib/08_etc_widget.dart
+++ ch02/lib/08_etc_widget.dart
@@ -74,6 +74,120 @@ class _MyHomePageState extends State<MyHomePage> {
             ],
           ),
 
+          // Card : 그림자가 있는 둥근 사각형 박스
+          Card(
+            elevation: 3,
+            color: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10)
+            ),
+            margin: EdgeInsets.all(12),
+            child: Container(
+              width: 200,
+              height: 100,
+              child: Column(
+                children: [
+                  Text('제목'),
+                  Text('내용')
+                ],
+              ),
+            ),
+          ),
+
+          // Padding
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: [
+                Text('padding 10')
+              ],
+            ),
+          ),
+
+          // Icon : 웹 아이콘 출력 위젯, google font 아이콘 확인
+          Row(
+            children: [
+              Icon(
+                Icons.home,
+                size: 60,
+              ),
+              Icon(
+                Icons.favorite,
+                size: 60,
+                color: Colors.red,
+              ),
+              Icon(
+                Icons.handshake,
+                size: 60,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+
+          // Spacer : 위젯과 위젯 사이의 빈공간 배치해서 비율 정렬
+          Row(
+            children: [
+              Icon(
+                Icons.home,
+                size: 60,
+              ),
+              Spacer(),
+              Icon(
+                Icons.favorite,
+                size: 60,
+                color: Colors.red,
+              ),
+              Spacer(
+                flex: 2,
+              ),
+              Icon(
+                Icons.handshake,
+                size: 60,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+
+          // SizedBox : 위젯과 위젯 사이에 여백을 줄때 사용하는 위젯
+          SizedBox(
+            width: 300,
+            height: 100,
+            child: Container(
+              color: Colors.blue,
+            ),
+          ),
+
+          // SingleChildScrollView
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Container(
+                  width: 400,
+                  height: 200,
+                  color: Colors.red,
+                  child: Text('Box1')
+                ),
+                Container(
+                    width: 400,
+                    height: 200,
+                    color: Colors.green,
+                    child: Text('Box2')
+                ),
+                Container(
+                    width: 400,
+                    height: 200,
+                    color: Colors.blue,
+                    child: Text('Box3')
+                )
+              ]
+            ),
+
+          ),
+
+          
+
+
         ],
       )
       
