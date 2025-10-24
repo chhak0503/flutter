37b07b68743bee2f2e98bc2c2ca83a6af7a5a3b9
chhak0503 <chhak0503@gmail.com>
2025-10-24 12:19:46 +0900

    add ch02

--- ch02/lib/03_image_widget.dart
+++ ch02/lib/03_image_widget.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Hello Flutter!'),
+      home: const MyHomePage(title: '02.Image Widget 실습'),
     );
   }
 }
@@ -34,15 +34,39 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Hello Flutter!'),
-            Text('Hello World!')
-          ],
-        ),
-      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          /*
+            이미지 에셋 등록 설정 ⭐⭐⭐
+             - pubspec.yaml > flutter 하위에 assets 주석 제거후 이미지 경로 입력
+          */
+          Image.asset('images/flower1.jpg'),
+          Image.asset(
+            'images/flower2.jpg',
+            width: 200,
+            height: 200,
+          ),
+          Image.asset(
+            'images/flower3.jpg',
+            width: 200,
+            height: 200,
+            fit: BoxFit.fill,
+            repeat: ImageRepeat.noRepeat,
+          ),
+          Image.network(
+            'https://picsum.photos/id/237/200/300',
+            width: 100,
+            height: 100,
+          ),
+          Image.network(
+            'https://picsum.photos/id/238/200/300',
+            width: 100,
+            height: 100,
+          )
+
+        ],
+      )
       
     );
   }
--- ch02/lib/04_container_widget.dart
+++ ch02/lib/04_container_widget.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Hello Flutter!'),
+      home: const MyHomePage(title: '03.Container Widget 실습'),
     );
   }
 }
@@ -34,15 +34,47 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Hello Flutter!'),
-            Text('Hello World!')
-          ],
-        ),
-      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: 200,
+            height: 200,
+            color: Colors.red,
+          ),
+          Container(
+            width: 200,
+            height: 200,
+            color: Colors.blue,
+            margin: EdgeInsets.only(left: 10, top: 10),
+            child: Text('Blue'),
+          ),
+          Container(
+            width: 200,
+            height: 200,
+            //color: Colors.green, // decoration 속성을 선언하면 color 속성과 충돌나서 에러
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.green,
+              border: Border.all(width: 1, color: Colors.black)
+            ),
+            child: Text('Green'),
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+              border: Border.all(width: 1, color: Colors.orange),
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                  image: AssetImage('images/flower1.jpg'),
+                  fit: BoxFit.cover
+              )
+            ),
+          ),
+
+        ],
+      )
       
     );
   }
